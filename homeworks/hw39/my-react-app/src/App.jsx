86f585bd2ad6a4ae3610de4af79cb858e7598969
PandEra <antonovmykola2008@gmail.com>
2026-03-06 00:22:33 +0100

    added hw39

---
+++ homeworks/hw39/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Definitions from "./components/Definitions.jsx";
+
+function App() {
+    const definitions = [
+        { dt: 'one', dd: 'two', id: 1 },
+        { dt: 'another term', dd: 'another description', id: 2 },
+    ];
+  return (
+    <Definitions data={definitions} />
+  )
+}
+
+export default App
