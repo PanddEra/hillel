de8f31eb72dcfd2108850b8163885c828fdb089b
PandEra <antonovmykola2008@gmail.com>
2026-03-19 20:20:53 +0100

    updated

---
+++ homeworks/hw36/js/models/UserModel.js
@@ -0,0 +1,12 @@
+class UserModel {
+    users = [];
+    #fetchGetAllUsers = () => {}
+    #fetchCreateUser = (userData) => {}
+    #fetchUpdateUser = (id, userData) => {}
+    #fetchDeleteUser = (id) => {}
+    
+    getAll(){}
+    create(userData){}
+    update(id, userData){}
+    delete(id){}
+}
